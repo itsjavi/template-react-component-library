faa8b43b90133afe3ad03ec0655de01a4c8781b4
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-08-31 19:25:59 +0200

    setup storybook

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,20 @@
 import type { ReactNode } from "react";
 
 export type ButtonProps = {
+  primary?: boolean;
   children: ReactNode;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
 };
 
 function Button({
+  primary = false,
   type = "button",
   children,
   onClick,
 }: ButtonProps): JSX.Element {
   return (
-    <button type={type} onClick={onClick}>
+    <button type={type} onClick={onClick} className={primary ? "primary" : ""}>
       {children}
     </button>
   );
