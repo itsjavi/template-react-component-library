72fcb99171ce76be03ec8460a0c96bc82c622fbf
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-08-31 19:11:46 +0200

    setup biome

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,21 +1,21 @@
 import type { ReactNode } from "react";
 
 export type ButtonProps = {
-	children: ReactNode;
-	type?: "button" | "submit" | "reset";
-	onClick?: () => void;
+  children: ReactNode;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
 };
 
 function Button({
-	type = "button",
-	children,
-	onClick,
+  type = "button",
+  children,
+  onClick,
 }: ButtonProps): JSX.Element {
-	return (
-		<button type={type} onClick={onClick}>
-			{children}
-		</button>
-	);
+  return (
+    <button type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export { Button };
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 // @see https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
-	plugins: [
-		react(),
-		dts({
-			tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
-		}),
-	],
-	build: {
-		lib: {
-			// Could also be a dictionary or array of multiple entry points
-			entry: resolve(__dirname, "src/index.ts"),
-			formats: ["es"],
-			fileName: "index",
-		},
-	},
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+  },
 });
