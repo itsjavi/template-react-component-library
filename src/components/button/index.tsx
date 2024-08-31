9772638230acb4addb993031d40e542682fd16ff
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-08-31 19:00:53 +0200

    configure lib mode with a simple component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react";
+
+export type ButtonProps = {
+	children: ReactNode;
+	type?: "button" | "submit" | "reset";
+	onClick?: () => void;
+};
+
+function Button({
+	type = "button",
+	children,
+	onClick,
+}: ButtonProps): JSX.Element {
+	return (
+		<button type={type} onClick={onClick}>
+			{children}
+		</button>
+	);
+}
+
+export { Button };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
+// @see https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		dts({
+			tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
+		}),
+	],
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points
+			entry: resolve(__dirname, "src/index.ts"),
+			formats: ["es"],
+			fileName: "index",
+		},
+	},
+});
